6f9835a0160362189b63c20229466822f865f10c
woojungyang <blossom1113@nate.com>
2024-06-11 21:28:22 +0900

    feature(master) : react-native-snap-carousel 파일 수정

--- components/TrendingMovies.js
+++ components/TrendingMovies.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions, Image, TouchableWithoutFeedback } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+
+const { width, height } = Dimensions.get('window');
 
 export default function TrendingMovies({ data }) {
   return (
     <View className="mb-8">
       <Text className="text-white text-xl mx-4 mb-5">trendingMovies</Text>
-      {/* <Carousel data={data} /> */}
+
+      <Carousel
+        data={data}
+        firstItem={1}
+        inactiveSlideOpacity={0.6}
+        sliderWidth={width}
+        itemWidth={width * 0.62}
+        slideStyle={{ display: 'flex', alignItems: 'center' }}
+        renderItem={({ item }) => <MovieCard item={item} />}
+      />
     </View>
   );
 }
+
+const MovieCard = ({ item }) => {
+  return (
+    <TouchableWithoutFeedback>
+      <Image
+        source={require('../assets/movie1.jpg')}
+        style={{ width: width * 0.6, height: height * 0.4 }}
+        className="rounded-3xl"
+      />
+    </TouchableWithoutFeedback>
+  );
+};
